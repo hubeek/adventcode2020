58d44b16101d0ce7bd36d3966f637535a37fbee8
HJHubeek <hjhubeek@gmail.com>
2021-01-04 22:34:33 +0100

    8 ready.

--- src/main/java/nl/appall/advent08.java
+++ src/main/java/nl/appall/advent08.java
@@ -6,24 +6,43 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import static java.lang.System.exit;
 
 public class advent08 {
 
     private static String bootcode;
+    private static int opsCounter = -1;
+    private static int opsValue = -1;
+    private static boolean opsChanged = false;
+    private static boolean found = false;
+
 
     public static void main(String[] args) throws IOException {
         System.out.println("Day - 8");
         System.out.println("=======");
         doPuzzleOne();
+        doPuzzleTwo();
     }
 
     private static void doPuzzleOne() throws IOException {
+        System.out.println("Puzzle 1");
         bootcode = new String(Files.readAllBytes(Paths.get("08 - bootcode.txt")));
-        List<String> operations = Arrays.asList(bootcode.split("\r\n"));
+        List<String> operations = Arrays.asList(bootcode.split("\n"));
         int accumulator = 0;
         run(operations, accumulator);
+
+    }
+
+    private static void doPuzzleTwo() throws IOException {
+        System.out.println("Puzzle 2");
+        bootcode = new String(Files.readAllBytes(Paths.get("08 - bootcode.txt")));
+        List<String> operations = Arrays.asList(bootcode.split("\n"));
+        int accumulator = 0;
+        var countops = operations.stream().filter(l -> l.contains("jmp") || l.contains("nop")).collect(Collectors.toList());
+
+        runWithChanger(operations, accumulator, countops.size());
 
     }
 
@@ -33,23 +52,49 @@ public class advent08 {
         doOperation(step, operations, accumulator, steps);
     }
 
-    private static void doOperation(int step, List<String> operations, int accumulator, List<Integer> steps) {
+
+    private static boolean doOperation(
+            int step,
+                    List<String> operations,
+                    int accumulator,
+                    List<Integer> steps)
+    {
+
+
+        if (step == operations.size()) {
+            System.out.println(String.format("end! acc: %d", accumulator));
+            found = true;
+            return found;
+        }
         if (step > operations.size()) {
+            System.out.println(String.format("step bigger than size %", step));
             step = step % operations.size();
         } else if (step < 0) {
             step = operations.size() + step;
         }
         if (steps.contains(step)) {
-            System.out.println(String.format("%s", accumulator));
-            exit(0);
+            if (opsValue == -1) {
+                System.out.println(String.format("is in loop! acc: %s", accumulator));
+                return true;
+            } else {
+                found = false;
+                return found;
+            }
+
         } else {
             steps.add(step);
         }
 
         String operationLine = operations.get(step);
-        System.out.println(String.format("%s %s", operationLine, accumulator));
+//        System.out.println(String.format("%s %s  %d", operationLine, accumulator, step));
         String operation = operationLine.split(" ")[0];
         String argument = operationLine.split(" ")[1];
+        if (opsChanged == false && opsValue != -1
+                && opsValue == opsCounter
+                && (operation.contains("jmp")||operation.contains("nop"))) {
+            operation = changeOperation(operation);
+        }
+        if (operation.contains("jmp")||operation.contains("nop")) opsCounter++;
         switch (operation) {
             case "nop":
                 step += 1;
@@ -68,6 +113,35 @@ public class advent08 {
                 System.out.println(operation);
                 break;
         }
+        return found;
     }
 
+    private static String changeOperation(String operation) {
+        opsChanged = !opsChanged;
+        if (operation.contains("jmp")) {
+            return operation.replace("jmp", "nop");
+        } else if (operation.contains("nop")) {
+            return operation.replace("nop", "jmp");
+        }
+        return operation;
+    }
+
+    public static void runWithChanger(List<String> operations, int accumulator, int countops) {
+        for (int i = 0; i < countops; i++) {
+//            System.out.println(String.format("%d try",i));
+            int step = 0;
+            List<Integer> steps = new ArrayList<>();
+            opsCounter = 0;
+            opsValue = i;
+            opsChanged = false;
+            accumulator = 0;
+            found = false;
+            if (doOperation(step, operations, accumulator, steps)) {
+                System.out.println(String.format("found at try %d ", i));
+                break;
+            } else {
+//                System.out.println("Not found!");
+            }
+        }
+    }
 }
--- src/test/java/nl/appall/advent08Test.java
+++ src/test/java/nl/appall/advent08Test.java
@@ -6,18 +6,19 @@ import java.io.IOException;
 import java.nio.file.Files;
 import java.nio.file.Paths;
 import java.util.*;
+import java.util.stream.Collectors;
 
 import static org.junit.Assert.assertNotNull;
 
 public class advent08Test {
 
     @Test
-    public void advent8created(){
+    public void advent8created() {
         assertNotNull(advent08.class);
     }
 
     @Test
-    public void exampleProgram(){
+    public void exampleProgram() {
         String program = "nop +0\n" +
                 "acc +1\n" +
                 "jmp +4\n" +
@@ -29,7 +30,41 @@ public class advent08Test {
                 "acc +6";
         List<String> operations = Arrays.asList(program.split("\n"));
         int accumulator = 0;
-        advent08.run(operations,accumulator);
+        advent08.run(operations, accumulator);
+    }
+
+    @Test
+    public void exampleProgramChangeNopJmp() {
+        String program = "nop +0\n" +
+                "acc +1\n" +
+                "jmp +4\n" +
+                "acc +3\n" +
+                "jmp -3\n" +
+                "acc -99\n" +
+                "acc +1\n" +
+                "jmp -4\n" +
+                "acc +6";
+        List<String> operations = Arrays.asList(program.split("\n"));
+        int accumulator = 0;
+        var countops = operations.stream().filter(l -> l.contains("jmp") || l.contains("nop")).collect(Collectors.toList());
+
+        advent08.runWithChanger(operations, accumulator, countops.size());
+    }
+
+    @Test
+    public void exampleProgram2Solution() {
+        String program = "nop +0\n" +
+                "acc +1\n" +
+                "jmp +4\n" +
+                "acc +3\n" +
+                "jmp -3\n" +
+                "acc -99\n" +
+                "acc +1\n" +
+                "nop -4\n" +
+                "acc +6";
+        List<String> operations = Arrays.asList(program.split("\n"));
+        int accumulator = 0;
+        advent08.run(operations, accumulator);
 
     }
 }
